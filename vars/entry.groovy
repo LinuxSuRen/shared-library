5ad11525363919fb6cbdbb533be4f706e5f12207
Zhao Xiaojie <rick@jenkins-zh.cn>
2020-04-22 16:47:24 +0800

    Update entry.groovy

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -1,6 +1,11 @@
 def container_x(String name, String condition, Closure body) {
-    def code = sh label: 'check container', returnStatus: true, script: condition
-
+    def code = 0
+    try {
+        code = sh label: 'check container', returnStatus: true, script: condition
+    } catch (e) {
+        code = -1
+    }
+    
     if (code == 0) {
         if(body) {
             body()
